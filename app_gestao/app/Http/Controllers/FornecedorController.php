401fa9943305011d48a9fe1fef37a536f7177f47
Trintim <victortrintim@gmail.com>
2021-09-11 22:49:55 -0300

    foreach testes

--- app_gestao/app/Http/Controllers/FornecedorController.php
+++ app_gestao/app/Http/Controllers/FornecedorController.php
@@ -11,18 +11,29 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '000.000.000-00'
+                'cnpj' => '0',
+                'ddd' => '11', //São paulo (SP)
+                'telefone' => '99999-9999'
             ],
             1 => [
                 'nome' => 'fornecedor 2',
                 'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', //Fortaleza (CE)
+                'telefone' => '00000-9999'
+            ],
+            2 => [
+                'nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '33', //Minas Gerais (MG)
+                'telefone' => '99999-0000'
             ],
         ];
 
+        /*
         $msg = isset($fornecedores[0]['cnpj']) ? "CNPJ informado!" : "CNPJ não informado!";
         echo  $msg;
-
-        /*
         condicao ? se verdadeiro : se falso;
         condicao ? se verdadeiro : (condicao ? se verdadeiro : se falso);
         */
--- app_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,6 +1,5 @@
 <body style="background-color: rgb(112, 5, 103);">
 
-
 <h3>Fornecedor</h3>
 
 {{-- Fica o comentario que será descartado pelo interpretador do blade --}}
@@ -24,60 +23,22 @@
 
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] }}
-        @empty($fornecedores[0]['cnpj'])
-            - Vazio
-        @endempty
-    @endisset
+
+    @forelse ($fornecedores as $fornecedor)
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <hr>
+    @empty
+        Não Existem fornecedores cadastrados
+    @endforelse ($fornecedores as $item => $fornecedor)
 @endisset
 
-{{--
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem varios fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
---}}
-
-{{-- @unless executa se o retorno for false --}}
-
-{{--
-@isset($fornecedores)
-
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[0]['cnpj'] }}
-    <br>
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    @isset($fornecedores[1]['cnpj'])
-        CNPJ: {{ $fornecedores[1]['cnpj'] }}
-    @endisset
-
-@endisset
---}}
 
 
-
-
-{{--
-@if( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor Inativo
-@endif
-<br>
-@unless ($fornecedores[0]['status'] == 'S') se o retorno da condição for false
-    Fornecedor Inativo
-@endunless --}}
 </body>
 
