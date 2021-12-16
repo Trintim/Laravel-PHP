710ae8b2887b68555a083091511bd44fa2d7df87
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2021-12-16 13:06:42 -0300

    test

--- app_gestao/app/Http/Controllers/PrincipalController.php
+++ app_gestao/app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,30 @@ class PrincipalController extends Controller
 {
     public function principal(){
         //echo 'Olá sejam bem-vindos!';
-        return view('site.principal');
+        $salario = 8000;
+        $novoSalario = NULL;
+
+        if($salario > 0 && $salario < 2000){
+
+            $novoSalario = $salario * 1.20;
+        }
+        if($salario >= 2000 && $salario < 4000){
+
+            $novoSalario = $salario * 1.15;
+        }
+        if($salario >= 4000 && $salario < 7000){
+
+            $novoSalario = $salario * 1.10;
+        }
+        if($salario >= 7000){
+
+            $novoSalario = $salario * 1.05;
+        }
+
+        echo $novoSalario;
+
+
+
+        //return view('site.principal');
     }
 }
